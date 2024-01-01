24dca67fc389a570a1a9d4f326918aed84506952
BartukyGames <75219663+BartukyGames@users.noreply.github.com>
2024-01-01 04:06:44 +0100

    Update fxmanifest.lua

--- flex-moneywash/fxmanifest.lua
+++ flex-moneywash/fxmanifest.lua
@@ -9,7 +9,8 @@ version "1.0.0"
 shared_scripts {
     'config.lua',
     '@qb-core/shared/locale.lua',
-    'locales/en.lua', -- change to what language you want
+    'locales/en.lua',
+    'locales/*.lua',
 }
 
 server_scripts {
