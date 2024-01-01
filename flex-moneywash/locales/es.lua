ea718ffd74baad97dce5230c1c97373833538992
BartukyGames <75219663+BartukyGames@users.noreply.github.com>
2024-01-01 04:10:22 +0100

    Create es.lua

---
+++ flex-moneywash/locales/es.lua
@@ -0,0 +1,30 @@
+local Translations = {
+  error = {
+      alreadymachine = 'Ya posees una máquina',
+      nocoin = 'No tienes una moneda...'
+  },
+  success = {
+      startedmachine = '¡Máquina encendida!',
+      startwash = 'Inicio del lavado',
+      getmoney = 'Tu lavado está listo y has ganado: €'
+  },
+  info = {
+      free = 'Libre',
+      bussy = 'Ocupado',
+      enterloc = ' para entrar',
+      leaveloc = ' para salir',
+      addcoin = ' insertar moneda',
+      startwash = ' para iniciar el lavado',
+      totalwashtimer = 'Tiempo restante de la máquina: ',
+      washtimer = 'Lavado listo en: ',
+  },
+  text = {
+ },
+
+if GetConvar('qb_locale', 'en') == 'es' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
